4c793cdd6c1e7974e23fd1085cddfb8edfbebebc
Paul Melinte <paul.melinte@softwire.com>
2021-07-01 14:37:00 +0300

    Added persistance for the click counter

--- marsappspa/src/components/ClickCounter.tsx
+++ marsappspa/src/components/ClickCounter.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const ClickCounter: React.FC = () => {
     const [clickCount, setClickCount] = useState(0);
-    
+
     function incrementCounter() {
         setClickCount(clickCount + 1);
     }
+
+    useEffect(() => {
+        if(clickCount === 0){
+            setClickCount(parseInt(localStorage.getItem("clickCount") || "0"));
+        }
+
+        localStorage.setItem("clickCount", clickCount.toString());
+    },[clickCount]);
 
     return (
         <div>
