2560fd5901d83bce359831b15a6fbc4ff7db089d
Paul Melinte <paul.melinte@softwire.com>
2021-07-01 16:44:53 +0300

    Added routing

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import { NASAinfo } from "./components/NASAinfo/NASAinfo";
 import { ClickCounter } from "./components/ClickCounters/ClickCounter";
@@ -6,13 +7,22 @@ import { ComplexClickCounter } from "./components/ClickCounters/ComplexClickCoun
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NASAinfo />
-        <ClickCounter />
-        <ComplexClickCounter />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+       <header className="App-header">
+          <Switch>
+            <Route path="/NASA"> <NASAinfo /> </Route>
+            <Route path="/clicker"> <ClickCounter /> </Route>
+            <Route path="/complexclicker"> <ComplexClickCounter /> </Route>
+            <Route path="/">
+              <Link to="/NASA" className="App-link"> NASA </Link>
+              <Link to="/clicker" className="App-link"> Clicker </Link>
+              <Link to="/complexclicker" className="App-link"> Complex Clicker </Link>
+            </Route>
+          </Switch>
+       </header>
+      </div>
+    </Router>
   );
 }
 
