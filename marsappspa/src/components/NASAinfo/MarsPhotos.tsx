507fb54ddd6cd1c2aba219893b13c0c3675c0679
Paul Melinte <paul.melinte@softwire.com>
2021-07-02 12:36:35 +0300

    Dynamic rover name select

--- marsappspa/src/components/NASAinfo/MarsPhotos.tsx
+++ marsappspa/src/components/NASAinfo/MarsPhotos.tsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Select from "react-select"
+import "./MarsPhotos.css"
 
 export const MarsPhotos: React.FC = () => {
-    const [roverApiData, setRoverApiData] = useState([]);
+    const [roverSelectApiData, setRoverSelectApiData] = useState<any[]>([]);
     const [selectedRover, setSelectedRover] = useState("");
     const [selectedCamera, setSelectedCamera] = useState("");
     const [selectedSol, setSelectedSol] = useState(0);
 
     const getRoverData = async () => {
         const roverData = await axios.get("http://localhost:8000/api/rovers/");
-        setRoverApiData(roverData.data);
-
         
+        const rovers: any[] = roverData.data;
+
+        const selectRovers = rovers.map(rover => ({
+            "value": rover,
+            "label": rover.name
+        }));
+
+        setRoverSelectApiData(selectRovers);
     };
 
     useEffect(() => {
@@ -21,10 +28,10 @@ export const MarsPhotos: React.FC = () => {
 
 
     return (
-        <form>
+        <form className="Mars-form">
             <label>
                 Select Rover:
-                <Select name="roverSelect" options={[]}/>
+                <Select name="roverSelect" options={roverSelectApiData} className="Mars-select"/>
             </label>
         </form>
     );
