3926f7f85b5bf15d03200dba132e9236938c1974
Paul Melinte <paul.melinte@softwire.com>
2021-07-05 10:22:21 +0300

    Added a flexbox to the photo display and some margins to the photos

--- marsappspa/src/components/PhotoDisplay.tsx
+++ marsappspa/src/components/PhotoDisplay.tsx
@@ -3,7 +3,7 @@ import "./PhotoDisplay.css"
 
 export const PhotoDisplay: React.FC<{photos: string[]}> = (props) => {
     return (
-        <div>
+        <div className="flex-container">
             <img src={props.photos[0]} className="photoDisplay-photo"/>
             <img src={props.photos[1]} className="photoDisplay-photo"/>
             <img src={props.photos[2]} className="photoDisplay-photo"/>
